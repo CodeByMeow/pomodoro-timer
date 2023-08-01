5b10ea608971627486c312ab44cbb2e9be1aaa36
7ColorsCat <tranquanki28@gmail.com>
2023-08-01 14:11:41 +0700

    (feat): duration status

---
+++ src/components/DurationStatus/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { Duration } from "../../configs/durationType";
+import { Heading } from "@chakra-ui/react";
+
+interface DurationStatusProps {
+    duration: Duration;
+}
+
+const DurationStatus: FC<DurationStatusProps> = ({ duration }) => {
+    return (
+        <Heading as={"h3"} textAlign={"center"} fontWeight={"medium"}>
+            {duration}
+        </Heading>
+    );
+};
+export default DurationStatus;
+
